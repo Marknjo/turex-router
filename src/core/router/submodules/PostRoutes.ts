67710ef1540163342eabad449bc5b3cae7023e71
Marknjo <marknjo7@gmail.com>
2022-06-25 15:31:26 +0300

    Basic impplementation of a post routes runner

---
+++ src/core/router/submodules/PostRoutes.ts
@@ -0,0 +1,77 @@
+import express, { Request, Response, Router } from 'express';
+// import { AppMetaKeys } from '../../types';
+// import { RouterConfigs } from '../core/RouterConfigs';
+import { RouterConfigsOptions } from '../types';
+
+export default class PostRoutes {
+  /**
+   * Store the current controller ID
+   */
+  private controllerId: string;
+
+  /**
+   * Stores alll the controller handlers
+   */
+  private routerStore: ControllerModel[] = [];
+
+  constructor(
+    private configs: RouterConfigsOptions,
+    private routeId: string,
+    private mergeParams: boolean
+  ) {
+    /// Get controller Id
+    this.controllerId = '';
+
+    // Reflect.getMetadata(
+    //   AppMetaKeys.CONTROLLER_ID,
+    //   RouterConfigs
+    // );
+
+    /// Assign controller store to controller handlers
+    this.routerStore = useCtrStore.findAll();
+  }
+
+  /**
+   * Initialize router
+   */
+  init() {
+    const router = express.Router({
+      mergeParams: this.mergeParams,
+      caseSensitive: !!this.configs.caseSensitive,
+      strict: !!this.configs.strict,
+    });
+
+    return router;
+  }
+
+  /**
+   *  Associate router with its controller
+   *  Handler router matching
+   */
+  matchRouteWithHandler(route: Router) {
+    this.routerStore.forEach(handler => {
+      /// We do all the magic of associations
+      const { controllerId, handlerFn, httpMethod, routeUrl } = handler;
+
+      //// Handle different routes
+      if (controllerId === this.controllerId) {
+        route[httpMethod](routeUrl, (req: Request, res: Response) => {
+          const handler = handlerFn as any;
+
+          res.send(handler());
+        });
+      }
+    });
+  }
+
+  /**
+   * Dispatch configured route
+   */
+  dispatchRoute(routeConfigs: Router) {
+    usePostRoutesStore.dispatch({
+      routeId: this.routeId,
+      baseUrl: this.configs.baseUrl,
+      router: routeConfigs,
+    });
+  }
+}
